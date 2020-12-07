69204def7cfce8f80faf2e65f8943278282359f1
JUDPro <vitaliyvlsdm@gmail.com>
2020-12-07 23:56:02 +0700

    Это только начало. Был добавлен компонент AppHeader.vue

--- judio/src/App.vue
+++ judio/src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
+    <AppHeader></AppHeader>
   </div>
 </template>
 
 <style>
 
 </style>
+
+<script>
+import AppHeader from './components/AppHeader';
+export default {
+  name: 'app',
+  components: {
+    AppHeader
+  }
+}
+</script>
---
+++ judio/src/components/AppHeader.vue
@@ -0,0 +1,43 @@
+<template>
+    <header class="header">
+        <div class="line-header">
+            <div class="avatar"></div>
+            <div class="user-name">User name</div>
+        </div>
+    </header>
+</template>
+
+<style>
+
+.header {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    height: 120px;
+}
+
+.line-header {
+    display: flex;
+    align-items: center;
+    width: 85%;
+    height: 100%;
+    border-bottom: 2px solid #505050;
+}
+
+.avatar {
+    background-color: #505050;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    margin-left: 30px;
+}
+
+.user-name {
+
+}
+
+</style>
+
+<script>
+export default {}
+</script>
