12c7a51afa080817dbfd1a57f52d8365a32c91e8
JUDPro <vitaliyvlsdm@gmail.com>
2020-12-08 17:37:09 +0700

    Добавлен firebase

---
+++ judio/src/plugins/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+let firebaseConfig = {
+    apiKey: "AIzaSyD4DU70XauUU1vjjiuDWPmwv5nk3bqgKDo",
+    authDomain: "judio-10aa1.firebaseapp.com",
+    projectId: "judio-10aa1",
+    storageBucket: "judio-10aa1.appspot.com",
+    messagingSenderId: "253779954642",
+    appId: "1:253779954642:web:8fd270f871bd7a9998d60b"
+};
+
+export default firebase.initializeApp(firebaseConfig);
