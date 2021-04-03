cda8a4e2ebb057022602b7e748c22376588173cd
JUDPro <vitaliyvlsdm@gmail.com>
2021-04-03 11:53:59 +0700

    +

--- src/App.vue
+++ src/App.vue
@@ -38,7 +38,6 @@ export default {
   width: 100%;
   height: 100%;
 }
-
 .openDialogAnimation-enter-active {
   transition: all .3s ease-in-out;
 }
--- src/views/AddVideo.vue
+++ src/views/AddVideo.vue
@@ -1,5 +1,14 @@
 <template>
-    <div>Add Video</div>
+    <div class="add-video">
+        <div class="container">
+            <div class="uploading-video"></div>
+            <div class="info-of-video">
+                <div class="name-video"></div>
+                <div class="description-video"></div>
+            </div>
+            <div class="video-editing"></div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -7,4 +16,17 @@ export default {}
 </script>
 
 <style>
+.add-video {
+    width: 95%;
+    height: 100%;
+    margin-left: 90px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.container {
+    width: auto;
+    height: 830px;
+    border: solid 1px #000;
+}
 </style>
