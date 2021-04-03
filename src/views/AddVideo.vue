dfd7bec6e35ad83d2db883c641e2de0a7ac5d760
JUDPro <vitaliyvlsdm@gmail.com>
2021-04-03 12:11:47 +0700

    +

--- src/views/AddVideo.vue
+++ src/views/AddVideo.vue
@@ -27,6 +27,34 @@ export default {}
 .container {
     width: auto;
     height: 830px;
+    display: flex;
+    flex-wrap: wrap;
+}
+.uploading-video {
+    width: 730px;
+    height: 415px;
     border: solid 1px #000;
+    margin-right: 30px;
+}
+.info-of-video {
+    width: 860px;
+    height: 415px;
+}
+.name-video {
+    width: 100%;
+    height: 50px;
+    border: solid 1px #000;
+}
+.description-video {
+    width: 100%;
+    height: 345px;
+    border: solid 1px #000;
+    margin-top: 20px;
+}
+.video-editing {
+    width: 100%;
+    height: 200px;
+    border: solid 1px #000;
+    align-self: flex-start;
 }
 </style>
