5c3fe9e4ea89b48898a8a256ad936beaa76e8a6a
JUDPro <vitaliyvlsdm@gmail.com>
2021-05-04 10:32:19 +0700

    the home will be ready soon!

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,78 +1,51 @@
 <template>
-    <div class="content-home">
-        <div class="video" v-for="video in userData.videos" :key="video.id">
-            <video 
-            :src="idVideo"
-            controls="controls"
-            loop="loop"
-            poster=""
-            @dragover.prevent=""
-            @drop.prevent="test"
-            autoplay
-            >{{video.id}}
-        </video>
-        </div>
+  <div class="content-home">
+    <div class="video" v-for="video in listVideo" :key="video.id">
+      <Judio :width="width" :height="height" :url_video="video">
+        {{ video.id }}
+      </Judio>
     </div>
+  </div>
 </template>
 
 <script>
-import {firebase} from '../plugins/firebase'
+import { firebase } from "../plugins/firebase";
+import Judio from "../components/Judio";
 
 export default {
-    name: 'Home',
-
-    data: () => ({
-        idVideo: '',
-        userData: { 
-            id: 0,
-            uid: '',
-            userEmail: '',
-            userAvatar: '',
-            videos: {
-                url: 'gs://judio-10aa1.appspot.com/16086600287720.webm'
-            }
-        },
-    }),
-
-    methods: {
-        writeData() {
-            return firebase.database().ref('users/testUser/userVideo/').get()
-        },
-        test(e) {
-            let i = e.dataTransfer.files[0]
-            let m = URL.createObjectURL(i)
-            this.idVideo = m
-        }
+  name: "Home",
+  components: {
+    Judio,
+  },
+  data: () => ({
+    listVideo: [],
+    width: "400px",
+    height: "250px",
+  }),
+  methods: {
+    async getData() {
+      await firebase
+        .firestore()
+        .collection("videos")
+        .get()
+        .then((snapshot) => {
+          snapshot.forEach((doc) => {
+            this.listVideo.push(doc.data().url);
+          });
+        });
     },
-    
-    mounted: function () {
-        this.writeData();
-    },
-    
-}
+  },
+  mounted() {
+    this.getData();
+  },
+};
 </script>
 
 <style>
 .content-home {
-    position: relative;
-    width: auto;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    flex-flow: wrap;
-    margin-left: 90px;
-}
-
-.video {
-    background-color: grey;
-    width: 380px;
-    height: 220px;
-    margin: 0px 0 0 0;
-}
-
-video {
-    width: 100%;
-    height: 100%;
+  display: grid;
+  grid-template-columns: auto auto auto;
+  justify-items: center;
+  margin-left: 90px;
 }
 </style>
